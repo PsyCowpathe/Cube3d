8430bd202683782835dbc1d96cd05acfb74db73d
Aurelien Girona <agirona@z4r7p5.42lyon.fr>
2021-06-04 20:22:07 +0200

    parsing 2.0

---
+++ propre/final/bonus/parsing.c
@@ -0,0 +1,274 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agirona <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/29 16:02:00 by agirona           #+#    #+#             */
+/*   Updated: 2021/06/04 20:21:52 by agirona          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+char	**get_file(char *str, int line)
+{
+	int		fd;
+	int		i;
+	char	**info;
+	char	*tmp;
+
+	fd = open(str, O_RDONLY);
+	i = 0;
+	info = malloc(sizeof(char *) * (line + 1));
+	if (info == NULL)
+		return (NULL);
+	while (get_next_line(fd, &tmp) == 1)
+		info[i++] = ft_strdup(tmp);
+	info[i] = ft_strdup("\0");
+	return (info);
+}
+
+char	**get_file_size(t_mlx *data, char *str)
+{
+	int		fd;
+	char	*tmp;
+
+	fd = open(str, O_RDONLY);
+	data->infoline = 0;
+	while (get_next_line(fd, &tmp) == 1)
+	{
+		free(tmp);
+		data->infoline++;
+	}
+	close(fd);
+	return (get_file(str, data->infoline));
+}
+
+int		error(t_mlx *data, int nb, int line)
+{
+	ft_putstr("Error\n");
+	if (nb == 1)
+		ft_putstr("Error empty line at start of file.");
+	if (nb == 2)
+	{
+		ft_putstr("Space at start of line : ");
+		ft_putnbr(line);
+	}
+	if (nb == 3)
+	{
+		ft_putstr("Space at end of line : ");
+		ft_putnbr(line);
+	}
+	if (nb == 4)
+		ft_putstr("Empty line at end of file.");
+	if (nb == 5)
+	{
+		ft_putstr("Empty line in map at line : ");
+		ft_putnbr(line);
+	}
+	if (nb == 6)
+	{
+		if (data->no == 0)
+			ft_putstr("Missing north texture.\n");
+		if (data->so == 0)
+			ft_putstr("Missing south texture.\n");
+		if (data->ea == 0)
+			ft_putstr("Missing east texture.\n");
+		if (data->we == 0)
+			ft_putstr("Missing west texture.\n");
+		if (data->f == 0)
+			ft_putstr("Missing floor color.\n");
+		if (data->c == 0)
+			ft_putstr("Missing ceiling color.\n");
+	}
+	if (nb == 7)
+	{
+		ft_putstr("Incorrect information at line : ");
+		ft_putnbr(line);
+	}
+	if (nb == 8)
+	{
+		if (nb == 0)
+			ft_putstr("Invalid caractere in floor color.");
+		else
+			ft_putstr("Invalid caractere in ceiling color.");
+	}
+	if (nb == 9)
+		ft_putstr("Color of floor and ceiling must be between 0 and 255.");
+	if (nb == 10 && line == 0)
+		ft_putstr("Missing space after 'F' in floor color.");
+	else if (nb == 10)
+		ft_putstr("Missing space after 'C' in ceiling color.");
+	if (nb == 11)
+	{
+		if (data->no > 1)
+			ft_putstr("Redefinition of north texture.\n");
+		if (data->so > 1)
+			ft_putstr("Redefinition of south texture.\n");
+		if (data->ea > 1)
+			ft_putstr("Redefinition of east texture.\n");
+		if (data->we > 1)
+			ft_putstr("Redefinition of west texture.\n");
+		if (data->f > 1)
+			ft_putstr("Redefinition of floor color.\n");
+		if (data->c > 1)
+			ft_putstr("Redefinition of ceiling color.\n");
+
+	}
+	return (0);
+}
+
+int		is_map(t_mlx *data, char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] && str[i] == ' ')
+		i++;
+	if (str[i] == 'N' && str[i + 1] == 'O')
+	{
+		data->no++;
+		data->north = ft_strdup(str + i + 2);
+		return (0);
+	}
+	if (str[i] == 'S' && str[i + 1] == 'O')
+	{
+		data->so++;
+		data->south = ft_strdup(str + i + 1);
+		return (0);
+	}
+	if (str[i] == 'W' && str[i + 1] == 'E')
+	{
+		data->we++;
+		data->west = ft_strdup(str + i + 1);
+		return (0);
+	}
+	if (str[i] == 'E' && str[i + 1] == 'A')
+	{
+		data->ea++;
+		data->east = ft_strdup(str + i + 1);
+		return (0);
+	}
+	if (str[i] == 'F')
+	{
+		data->f++;
+		data->floor = ft_strdup(str + i + 1);
+		return (0);
+	}
+	if (str[i] == 'C')
+	{
+		data->c++;
+		data->ceiling = ft_strdup(str + i + 1);
+		return (0);
+	}
+	return (1);
+}
+
+int		hav_path(t_mlx *data)
+{
+	if (data->no > 0 && data->so > 0 && data->ea > 0
+		&& data->we > 0 && data->f > 0 && data->c > 0)
+		return (1);
+	return (0);	
+}
+
+int		verif_form(char **info, t_mlx *data)
+{
+	int		i;
+	int		c;
+	int		map;
+
+	i = -1;
+	if (info[i + 1][0] == '\0')
+		return (error(data, 1, -1));
+	map = 0;
+	while (++i <= data->infoline)
+	{
+		if (info[i][0] == ' ' && hav_path(data) == 0)
+			return (error(data, 2, i + 1));
+		if (hav_path(data) == 1 && info[i][0] != '\0')
+			map = 1;
+		if (map == 1 && info[i][0] == '\0' && hav_path(data) == 1 && i != data->infoline)
+			return (error(data, 5, i + 1));
+		if (is_map(data, info[i]) == 1 && info[i][0] != '\0' && hav_path(data) == 0)
+			return (error(data, 7, i + 1));
+		if (i == data->infoline - 1 && info[i][0] == '\0')
+			return (error(data, 4, -1));
+		c = -1;
+		while (info[i][++c] != '\0')
+		{
+			ft_putchar(info[i][c]);
+			if (info[i][c + 1] == '\0' && info[i][c] == ' ')
+				return (error(data, 3, i + 1));
+			if (map == 1 && ft_ischar(" 012NSEW", info[i][c]) == 0)
+				return (error(data, 7, i + 1));
+		}
+		ft_putchar('\n');
+	}
+	if (hav_path(data) == 0)
+		return (error(data, 6, -1));
+	return (1);
+}
+
+int		verif_info(t_mlx *data, char *str, int invalid)
+{
+	int	i;
+	int	c;
+	int	rgb;
+	char	tmp[4];
+
+	i = 0;
+	rgb = 0;
+	while (str[i] == ' ')
+		i++;
+	if (i == 0)
+		return (error(data, 10, invalid));
+	while (str[i])
+	{
+		if (ft_ischar("0123456789", str[i]) == 0)
+			return (error(data, 8, invalid));
+		c = 0;
+		while (str[i] && ft_ischar("0123456789", str[i]) == 1)
+			tmp[c++] = str[i++];
+		tmp[c] = '\0';
+		data->frgb[rgb] = ft_atoi(tmp);
+		if (!(data->frgb[rgb] >= 0 && data->frgb[rgb] <= 255))
+			return (error(data, 9, -1));
+		if (rgb != 2 && !(str[i] == ',' && (str[i + 1] == ' '
+		|| ft_ischar("0123456789", str[i + 1]))))
+			return (error(data, 8, invalid));
+		while (rgb != 2 && str[i] && ft_ischar("0123456789", str[i]) == 0)
+			i++;
+		if (rgb == 2 && i != (int)ft_strlen(str))
+			return (error(data, 8, invalid));
+		rgb++;
+	}
+	return (1);
+}
+
+int		parsing(t_mlx *data, char *str)
+{
+	char	**info;
+
+	data->north = 0;
+	data->south = 0;
+	data->east = 0;
+	data->west = 0;
+	data->floor = 0;
+	data->ceiling = 0;
+	info = get_file_size(data, str);
+	if (info == NULL)
+		return (0);
+	if (verif_form(info, data) == 0)
+		return (0);
+	if (data->no > 1 || data->so > 1 || data->ea > 1
+		|| data->we > 1 || data->f > 1 || data->c > 1)
+		return (error(data, 11, -1));
+	if (verif_info(data, data->floor, 0) == 0)
+		return (0);
+	if (verif_info(data, data->ceiling, 1) == 0)
+		return (0);
+	return (1);
+}
---
+++ propre/final/bonus/raycasting.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycasting.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agirona <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/04 14:28:35 by agirona           #+#    #+#             */
+/*   Updated: 2021/05/25 20:58:13 by agirona          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	get_first_hor(t_mlx *data, float ray)
+{
+	int		tmp;
+
+	tmp = floor(data->py / data->cubesize);
+	if (ray >= 0 && ray < 180)
+		data->hory = tmp * data->cubesize - 0.000001;
+	else if (ray >= 180 && ray <= 360)
+		data->hory = tmp * data->cubesize + data->cubesize;
+	data->hory = data->hory / data->cubesize;
+	data->horx = data->px + (data->py - data->hory) / tan(ray * (M_PI / 180));
+}
+
+void	get_first_vert(t_mlx *data, float ray)
+{
+	int		tmp;
+
+	tmp = floor(data->px / data->cubesize);
+	if ((ray > 0 && ray <= 90) || (ray > 270 && ray <= 360))
+		data->vertx = tmp * data->cubesize + data->cubesize;
+	else
+		data->vertx = tmp * data->cubesize - 0.000001;
+	data->vertx = data->vertx / data->cubesize;
+	data->verty = data->py + (data->px - data->vertx) * tan(ray * (M_PI / 180));
+}
+
+void	get_closest_vertical_wall(t_mlx *data, float ray)
+{
+	float	add;
+
+	add = tan(ray * (M_PI / 180));
+	while (data->vertx >= 0 && data->verty < data->mapx
+		&& data->verty >= 0 && data->verty < data->mapy
+		&& ft_ischar("02", data->map[(int)data->verty][(int)data->vertx]))
+	{
+		if ((ray >= 0 && ray < 90) || (ray >= 270 && ray <= 360))
+		{
+			data->vertx = data->vertx + data->cubesize;
+			data->verty = data->verty - add;
+		}
+		else
+		{
+			data->vertx = data->vertx - data->cubesize - 0.000001;
+			data->verty = data->verty + add;
+		}
+	}
+	data->vdist = sqrt(pow(data->vertx - data->px, 2)
+			+ pow(data->verty - data->py, 2));
+}
+
+void	get_closest_horizontal_wall(t_mlx *data, float ray)
+{
+	float	add;
+
+	add = 1 / tan(ray * (M_PI / 180));
+	while (data->horx >= 0 && data->horx <= data->mapx
+		&& data->hory >= 0 && data->hory <= data->mapy
+		&& ft_ischar("02", data->map[(int)data->hory][(int)data->horx]))
+	{
+		if ((ray >= 0 && ray < 90) || (ray >= 90 && ray < 180))
+		{
+			data->hory = data->hory - data->cubesize - 0.000001;
+			data->horx = data->horx + add;
+		}
+		else
+		{
+			data->hory = data->hory + data->cubesize;
+			data->horx = data->horx - add;
+		}
+	}
+	data->hdist = sqrt(pow(data->horx - data->px, 2)
+			+ pow(data->hory - data->py, 2));
+}
+
+void	get_closest_wall(t_mlx *data, int x, float patch)
+{
+	float	wallheight;
+	float	distance;
+	int		texture;
+
+	if (data->vdist < data->hdist)
+		distance = data->vdist *  cos(patch * (M_PI / 180));
+	else
+		distance = data->hdist *  cos(patch * (M_PI / 180));
+	wallheight = data->cubesize / distance * data->proj;
+	if (data->hdist <= data->vdist)
+	{
+		data->alldist[x] = data->hdist;
+		texture = 0;
+		if (data->hory >= (int)data->hory + 0.5)
+			texture = 1;
+	}
+	else
+	{
+		data->alldist[x] = data->vdist;
+		texture = 3;
+		if (data->vertx >= (int)data->vertx + 0.5)
+			texture = 2;
+	}
+	print_column(data, x, wallheight, texture);
+}
